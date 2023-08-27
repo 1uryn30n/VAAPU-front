0f09a541a9e2c6f108ea11727271d5c66d858923
1uryn30n <alysson123iury@hotmail.com>
2023-08-26 22:31:23 -0400

    adicionadoLogin

--- trabalhoagil.js
+++ trabalhoagil.js
@@ -7,6 +7,7 @@ menu.onclick = () => {
     navbar.classList.toggle('active');
     menu.classList.toggle('move');
     carrinho.classList.remove('active');
+    login.classList.remove('active');
 }
  // carrinho verificar 
 
@@ -16,4 +17,15 @@ menu.onclick = () => {
     carrinho.classList.toggle('active');
     navbar.classList.remove('active');
     menu.classList.remove('move');
+    login.classList.remove('active');
+}
+
+// fazer login
+let login = document.querySelector('.login-form');
+
+document.querySelector('#user-icon').onclick =() => {
+   login.classList.toggle('active');
+    carrinho.classList.remove('active');
+   navbar.classList.remove('active');
+   menu.classList.remove('move');
 }
